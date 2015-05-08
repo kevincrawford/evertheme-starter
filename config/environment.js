cfdd4e5c8993763850b3d98f8891a9e9663548a6
Kevin Crawford <git@kevin-crawford.com>
2015-05-08 09:17:44 -0500

    Initial Release

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'starter',
+    modulePrefix: 'evertheme',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
